dbe5ecee9b911476758cb2820cf36964aee12b75
iter.dan <iter.dan@885d0eb7-8f28-6eb0-42c6-4fe6b8a6ad53>
2010-10-13 00:46:39 +0000

    implemented connectLights() method

--- trunk/src/mosquito/g1/geometries/LightConfiguration.java
+++ trunk/src/mosquito/g1/geometries/LightConfiguration.java
@@ -3,8 +3,7 @@ package mosquito.g1.geometries;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LightConfiguration {
     private List<Point2D> lightSet;
@@ -41,8 +40,45 @@ public class LightConfiguration {
     /**
      * @return The paths that will be traveled by mosquitoes to the center light.
      */
-    protected Set<Line2D> connectLights() {
-        return null;
+    public Set<Line2D> connectLights() 
+    {
+    	Set<Line2D> lines = new HashSet<Line2D>();
+    	ArrayList<Point2D> unused = new ArrayList<Point2D>(lightSet);
+    	Point2D center = lightSet.get(centerLightIndex);
+    	//remove center light
+    	for(Point2D p : unused)
+    	{
+    		if(p.equals(center));
+    			unused.remove(p);
+    	}
+    	
+    	//find all lines connected to center light
+    	for(Point2D p : unused)
+    	{
+    		if(p.distance(center) <= 25)
+    		{
+    			lines.add(new Line2D.Double(p,center));
+    			unused.remove(p);
+    		}
+    	}
+    	
+    	while(!unused.isEmpty())
+    	{
+    		Point2D temp = unused.get(0);
+    		unused.remove(temp);
+    		for(Point2D p : lightSet)
+        	{
+    			if(p.equals(temp))
+    				continue;
+        		if(p.distance(temp) <= 25)
+        		{
+        			lines.add(new Line2D.Double(p,temp));
+        			unused.remove(p);
+        		}
+        	}	
+    	}
+    	
+        return lines;
     }
     
     /**
