c6bd42b3654b6b7f4d609369e6459519d836b522
zackzachariah@gmail.com <zackzachariah@gmail.com@885d0eb7-8f28-6eb0-42c6-4fe6b8a6ad53>
2010-10-16 22:55:07 +0000

    Fixed a stupid, stupid bug on my part

--- trunk/src/mosquito/g1/geometries/LightConfiguration.java
+++ trunk/src/mosquito/g1/geometries/LightConfiguration.java
@@ -86,6 +86,7 @@ public class LightConfiguration {
                 boolean foundOne = false;
                 for(int i = 0; i < depths.length; i++) {
                     if(depths[i] == walkOut) {
+                        foundOne = true;
                         Set<Integer> endPoints = edges.get(i);
                         for(Integer endPoint : endPoints) {
                             depths[endPoint] = Math.min(depths[endPoint], (walkOut+1));
